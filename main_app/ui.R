829cd71a578a02e82783f1a25d25ece7d9239a76
Noemi Montobbio <noemi.montobbio@gmail.com>
2026-02-10 16:35:08 +0100

    new features banner

--- main_app/ui.R
+++ main_app/ui.R
@@ -104,7 +104,7 @@ fluidPage(
     style = "margin: 10px 0;",
     icon("bullhorn"),
     tags$strong(" What's new  "),
-    HTML("<br>Support for numeric \"day\" column as a substitute of calendar dates") #,
+    HTML("<br>Support for numeric \"day\" column as a substitute for calendar dates") #,
     # tags$a(
     #   href = "https://github.com/noemimontobbio/msprog/blob/master/msprog.pdf",
     #   target = "_blank",
